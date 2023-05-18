d3742a75abaa1520e5cd8e277755461f66acaf92
GillesClsns <gilles.claessens@student.kdg.be>
2023-05-18 11:25:39 +0200

    Updated 3.1

--- js/form_validation.js
+++ js/form_validation.js
@@ -15,7 +15,7 @@ function init() {
     let serverPasswordInput = document.getElementById("server-password");
 
     form.addEventListener("submit", formSubmit);
-    nameInput.addEventListener("change", validateName);
+    nameInput.addEventListener("input", validateName);
     emailInput.addEventListener("input", validateEmail);
     billingPeriodSelect.addEventListener("input", validateBillingPeriod);
     playerSlotsInput.addEventListener("input", validatePlayerSlots);
@@ -67,35 +67,33 @@ function formSubmit(event) {
 function validateName(event) {
     let nameInput = event.target;
     let value = nameInput.value.trim();
-    let errorSpan = document.getElementById(nameInput.dataset.error);
 
-    if (value.length > 0 && !value.includes(" ")) {
-        errorSpan.textContent = "";
+    let regex = /^\S.*\S$/;
+
+    if (value.length > 0 && regex.test(value)) {
         nameInput.classList.add("valid-input");
         nameInput.classList.remove("invalid-input");
     } else {
-        errorSpan.textContent = nameInput.dataset.errorMessage;
         nameInput.classList.remove("valid-input");
         nameInput.classList.add("invalid-input");
     }
 }
+
 
 /**
  * Validates the email input field.
  * @param {{target: HTMLElement}} event - The input change event.
  */
 function validateEmail(event) {
+
     let emailInput = event.target;
     let value = emailInput.value.trim();
     let pattern = /^[A-Za-z]+\.[A-Za-z]+@student\.kdg\.be$/;
-    let errorSpan = document.getElementById(emailInput.dataset.error);
 
-    if (pattern.test(value)) {
-        errorSpan.textContent = "";
+    if (value.length > 0 && pattern.test(value)) {
         emailInput.classList.add("valid-input");
         emailInput.classList.remove("invalid-input");
     } else {
-        errorSpan.textContent = emailInput.dataset.errorMessage;
         emailInput.classList.remove("valid-input");
         emailInput.classList.add("invalid-input");
     }
@@ -108,14 +106,11 @@ function validateEmail(event) {
 function validateAddress(event) {
     let addressInput = event.target;
     let value = addressInput.value.trim();
-    let errorSpan = document.getElementById(addressInput.dataset.error);
 
     if (value.length > 0) {
-        errorSpan.textContent = "";
         addressInput.classList.add("valid-input");
         addressInput.classList.remove("invalid-input");
     } else {
-        errorSpan.textContent = addressInput.dataset.errorMessage;
         addressInput.classList.remove("valid-input");
         addressInput.classList.add("invalid-input");
     }
@@ -128,14 +123,11 @@ function validateAddress(event) {
 function validateCity(event) {
     let cityInput = event.target;
     let value = cityInput.value.trim();
-    let errorSpan = document.getElementById(cityInput.dataset.error);
 
     if (value.length > 0) {
-        errorSpan.textContent = "";
         cityInput.classList.add("valid-input");
         cityInput.classList.remove("invalid-input");
     } else {
-        errorSpan.textContent = cityInput.dataset.errorMessage;
         cityInput.classList.remove("valid-input");
         cityInput.classList.add("invalid-input");
     }
@@ -146,16 +138,14 @@ function validateCity(event) {
  * @param {{target: HTMLElement}} event - The input change event.
  */
 function validateState(event) {
+
     let stateInput = event.target;
     let value = stateInput.value.trim();
-    let errorSpan = document.getElementById(stateInput.dataset.error);
 
     if (value.length > 0) {
-        errorSpan.textContent = "";
         stateInput.classList.add("valid-input");
         stateInput.classList.remove("invalid-input");
     } else {
-        errorSpan.textContent = stateInput.dataset.errorMessage;
         stateInput.classList.remove("valid-input");
         stateInput.classList.add("invalid-input");
     }
@@ -169,14 +159,11 @@ function validateZip(event) {
     let zipInput = event.target;
     let value = zipInput.value.trim();
     let pattern = /^\d{4}$/;
-    let errorSpan = document.getElementById(zipInput.dataset.error);
 
-    if (pattern.test(value)) {
-        errorSpan.textContent = "";
+    if (value.length > 0 && pattern.test(value)) {
         zipInput.classList.add("valid-input");
         zipInput.classList.remove("invalid-input");
     } else {
-        errorSpan.textContent = zipInput.dataset.errorMessage;
         zipInput.classList.remove("valid-input");
         zipInput.classList.add("invalid-input");
     }
@@ -188,13 +175,10 @@ function validateZip(event) {
  */
 function validateBillingPeriod() {
     let billingPeriodSelect = document.getElementById("billing-period");
-    let errorSpan = document.getElementById("billing-period-error");
 
     if (billingPeriodSelect.value !== "") {
-        errorSpan.textContent = "";
         billingPeriodSelect.classList.add("valid-input");
     } else {
-        errorSpan.textContent = "Please select a billing period.";
         billingPeriodSelect.classList.remove("valid-input");
     }
 }
@@ -204,13 +188,10 @@ function validateBillingPeriod() {
  */
 function validatePlayerSlots() {
     let playerSlotsInput = document.getElementById("player-slots");
-    let errorSpan = document.getElementById("player-slots-error");
 
     if (playerSlotsInput.value >= 1 && playerSlotsInput.value <= 100) {
-        errorSpan.textContent = "";
         playerSlotsInput.classList.add("valid-input");
     } else {
-        errorSpan.textContent = "Player slots must be between 1 and 100.";
         playerSlotsInput.classList.remove("valid-input");
     }
 }
@@ -220,13 +201,10 @@ function validatePlayerSlots() {
  */
 function validateServerName() {
     let serverNameInput = document.getElementById("server-name");
-    let errorSpan = document.getElementById("server-name-error");
 
     if (serverNameInput.value.trim().length > 0) {
-        errorSpan.textContent = "";
         serverNameInput.classList.add("valid-input");
     } else {
-        errorSpan.textContent = "Please enter a server name.";
         serverNameInput.classList.remove("valid-input");
     }
 }
@@ -236,13 +214,10 @@ function validateServerName() {
  */
 function validateServerPassword() {
     let serverPasswordInput = document.getElementById("server-password");
-    let errorSpan = document.getElementById("server-password-error");
 
     if (serverPasswordInput.value.length >= 8) {
-        errorSpan.textContent = "";
         serverPasswordInput.classList.add("valid-input");
     } else {
-        errorSpan.textContent = "Password must be at least 8 characters long.";
         serverPasswordInput.classList.remove("valid-input");
     }
 }
